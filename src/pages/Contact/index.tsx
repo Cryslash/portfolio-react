3eb1404d7449279bee0109fef9bc35952a7ff852
Crystofher Lins <crystofher_lins@outlook.com>
2025-07-03 13:24:23 -0300

    contact page updated

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -2,47 +2,42 @@ import { Instagram, Linkedin, Mail, MapPin, PhoneCall } from 'lucide-react';
 import { Container } from '../../components/Container';
 import styles from './styles.module.css';
 import { AnimatedTitle } from '../../components/AnimatedTitle';
-import { AnimatedContent } from '../../components/AnimatedContent';
 import { ContactCard } from '../../components/ContactCard';
 
 export function Contact() {
-  //TODO: Data Array !!
+  const contactData = [
+    {
+      desc: 'telefone',
+      icon: <PhoneCall className={styles.icon} />,
+    },
+    {
+      desc: 'email',
+      info: 'crystofher_lins@outlook.com',
+      href: 'mailto:crystofher_lins@outlook.com',
+      icon: <Mail className={styles.icon} />,
+    },
+    {
+      desc: 'localização',
+      info: 'Brasil - Ceará',
+      icon: <MapPin className={styles.icon} />,
+    },
+    {
+      desc: 'linkedin',
+      href: 'https://www.linkedin.com/in/crystofher-lins-15aa69248',
+      icon: <Linkedin className={styles.icon} />,
+    },
+    {
+      desc: 'instagram',
+      href: 'https://www.instagram.com/crystofher_lins/',
+      icon: <Instagram className={styles.icon} />,
+    },
+  ];
 
   return (
     <Container>
       <AnimatedTitle className={styles.title}>Contatos</AnimatedTitle>
 
-      <AnimatedContent className={styles.contentFluid}>
-        <ContactCard desc='telefone'>
-          <PhoneCall className={styles.icon} />
-        </ContactCard>
-
-        <ContactCard
-          desc='email'
-          info='crystofher_lins@outlook.com'
-          href='mailto:crystofher_lins@outlook.com'
-        >
-          <Mail className={styles.icon} />
-        </ContactCard>
-
-        <ContactCard desc='localização' info='Brasil - Ceará'>
-          <MapPin className={styles.icon} />
-        </ContactCard>
-
-        <ContactCard
-          desc='linkedin'
-          href='https://www.linkedin.com/in/crystofher-lins-15aa69248'
-        >
-          <Linkedin className={styles.icon} />
-        </ContactCard>
-
-        <ContactCard
-          desc='instagram'
-          href='https://www.instagram.com/crystofher_lins/'
-        >
-          <Instagram className={styles.icon} />
-        </ContactCard>
-      </AnimatedContent>
+      <ContactCard data={contactData}></ContactCard>
     </Container>
   );
 }
