288bb574c2e03f5e8ab147095b5f97bcdfd51c21
Crystofher Lins <crystofher_lins@outlook.com>
2025-07-05 15:49:29 -0300

    skills page updated

--- src/pages/Skills/index.tsx
+++ src/pages/Skills/index.tsx
@@ -1,59 +1,11 @@
 import { AnimatedTitle } from '../../components/AnimatedTitle';
 import { Container } from '../../components/Container';
 import { Technologies } from '../../components/Technologies';
+import { TechData } from '../../data/skills';
 import { MainTemplate } from '../../templates/MainTemplate';
 import styles from './styles.module.css';
 
 export function Skills() {
-  const techData = [
-    { alt: 'React', title: 'React', src: '/images/icons/react.svg' },
-    {
-      alt: 'TypeScript',
-      title: 'TypeScript',
-      src: '/images/icons/ts.svg',
-    },
-    { alt: 'JavaScript', title: 'JavaScript', src: '/images/icons/js.svg' },
-    { alt: 'Vite', title: 'Vite', src: '/images/icons/vite.svg' },
-    { alt: 'NextJs', title: 'NextJs', src: '/images/icons/nextjs.svg' },
-    { alt: 'NodeJs', title: 'NodeJs', src: '/images/icons/node.svg' },
-
-    { alt: 'Rails', title: 'RubyOnRails', src: '/images/icons/rails.svg' },
-    { alt: 'PHP', title: 'PHP', src: '/images/icons/php.svg' },
-    {
-      alt: 'CodeIgniter',
-      title: 'CodeIgniter',
-      src: '/images/icons/codeIgniter.svg',
-    },
-    {
-      alt: 'TailwindCSS',
-      title: 'TailwindCSS',
-      src: '/images/icons/tailwindCss.svg',
-    },
-    {
-      alt: 'Bootstrap',
-      title: 'Bootstrap',
-      src: '/images/icons/bootstrap.svg',
-    },
-    { alt: 'HTML5', title: 'HTML5', src: '/images/icons/html.svg' },
-    { alt: 'CSS', title: 'CSS', src: '/images/icons/css3.svg' },
-
-    { alt: 'C#', title: 'C#', src: '/images/icons/cSharp.svg' },
-    { alt: 'Python', title: 'Python', src: '/images/icons/python.svg' },
-    { alt: 'Lua', title: 'Lua', src: '/images/icons/lua.svg' },
-    { alt: 'MongoDB', title: 'MongoDB', src: '/images/icons/mongo.svg' },
-    { alt: 'Sqlite', title: 'Sqlite', src: '/images/icons/sqlite.svg' },
-    { alt: 'MySQL', title: 'MySQL', src: '/images/icons/mySql.svg' },
-
-    {
-      alt: 'SQLServer',
-      title: 'SQLServer',
-      src: '/images/icons/sqlServer.svg',
-    },
-    { alt: 'WSL', title: 'WSL', src: '/images/icons/wsl.svg' },
-    { alt: 'Docker', title: 'Docker', src: '/images/icons/docker.svg' },
-    { alt: 'Git', title: 'Git', src: '/images/icons/git.svg' },
-  ];
-
   return (
     <MainTemplate>
       <Container>
@@ -61,7 +13,7 @@ export function Skills() {
           <AnimatedTitle className={styles.langTitle}>
             Algumas das tecnologias que utilizo
           </AnimatedTitle>
-          <Technologies data={techData} />
+          <Technologies data={TechData} />
         </div>
       </Container>
     </MainTemplate>
