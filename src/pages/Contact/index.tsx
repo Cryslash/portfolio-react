4a965e22b522f239530cb392aed17c3c9d889991
Crystofher Lins <crystofher_lins@outlook.com>
2025-07-05 15:50:37 -0300

    contact page updated

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,43 +1,15 @@
-import { Instagram, Linkedin, Mail, MapPin, PhoneCall } from 'lucide-react';
 import { Container } from '../../components/Container';
 import styles from './styles.module.css';
 import { AnimatedTitle } from '../../components/AnimatedTitle';
 import { ContactCard } from '../../components/ContactCard';
+import { ContactData } from '../../data/contacts';
 
 export function Contact() {
-  const contactData = [
-    {
-      desc: 'telefone',
-      icon: <PhoneCall className={styles.icon} />,
-    },
-    {
-      desc: 'email',
-      info: 'crystofher_lins@outlook.com',
-      href: 'mailto:crystofher_lins@outlook.com',
-      icon: <Mail className={styles.icon} />,
-    },
-    {
-      desc: 'localização',
-      info: 'Brasil - Ceará',
-      icon: <MapPin className={styles.icon} />,
-    },
-    {
-      desc: 'linkedin',
-      href: 'https://www.linkedin.com/in/crystofher-lins-15aa69248',
-      icon: <Linkedin className={styles.icon} />,
-    },
-    {
-      desc: 'instagram',
-      href: 'https://www.instagram.com/crystofher_lins/',
-      icon: <Instagram className={styles.icon} />,
-    },
-  ];
-
   return (
     <Container>
       <AnimatedTitle className={styles.title}>Contatos</AnimatedTitle>
 
-      <ContactCard data={contactData}></ContactCard>
+      <ContactCard data={ContactData}></ContactCard>
     </Container>
   );
 }
